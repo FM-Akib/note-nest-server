b52daf71ab6922aa002c096287019131bf10e9ef
FM-Akib <muntasirakib08@gmail.com>
2024-05-12 14:11:20 +0600

    bug fixed

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const cors = require('cors')
-require('dotenv').config()
+const cors = require('cors');
+require('dotenv').config();
+
+
 const port = process.env.PORT || 5000;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-//middleware
-app.use(cors())
-app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.245gucn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.245gucn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,14 +27,14 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-    await client.connect();
+   
 
 
    const usersCollection = await client.db('iiucResources').collection("users");
 
 
      //users
-     app.get('/users',async(req, res)=>{
+      app.get('/users',async(req, res)=>{
         const result = await usersCollection.find().toArray();
         res.send(result);
        })
@@ -47,19 +49,20 @@ async function run() {
         const result = await usersCollection.insertOne(user);
         res.send(result);
        })
-
-
-
-
-
-
-
-
    
-    await client.db("admin").command({ ping: 1 });
-    console.log("Note Nest is Chilling!");
+ 
+      await client.connect();
+      await client.db("admin").command({ ping: 1 });
+
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
+app.get('/', (req, res) => {
+  res.send('Note Nest Server is running!!')
+})
+
+app.listen(port, () => {
+  console.log(`Note Nest listening on port ${port}`)
+})
