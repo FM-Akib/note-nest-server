5270bf4d7a9994daa379fc46d706a768d640f866
FM-Akib <muntasirakib08@gmail.com>
2024-06-11 13:45:40 +0600

    updated course edit

--- index.js
+++ index.js
@@ -32,14 +32,17 @@ async function run() {
 
    const usersCollection = await client.db('iiucResources').collection("users");
    const courseCollection = await client.db('iiucResources').collection("cseCourse");
+   const clubsCollection = await client.db('iiucResources').collection("clubs");
 
 
-    //users
+    //users======================================================================================
+    // all user data
     app.get('/users',async(req, res)=>{
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
 
+    //  a user data
     app.get('/users/:email',async(req, res)=>{
       const {email} = req.params;
       const query = {email}
@@ -47,7 +50,7 @@ async function run() {
       res.send(result);
     })
 
-     
+    //  register a user
     app.post('/users',async(req, res)=>{
       const user = req.body;
       const query = {email: user.email}
@@ -59,6 +62,8 @@ async function run() {
       res.send(result);
     })
 
+
+    // adding user contribution 
     app.patch('/users/:email', async (req, res) => {
       const { email } = req.params;
       const {  resource } = req.body;
@@ -80,6 +85,38 @@ async function run() {
       }
   });
   
+
+
+  
+  //  a user edit his/her contribution data
+  
+  app.patch('/users/:email/contribution/:id', async (req, res) => {
+    const email = req.params.email;
+    const contributionId = req.params.id;
+    const { resource } = req.body;
+   
+    try {
+      const result = await usersCollection.updateOne(
+        { email: email, "myContribution.id": contributionId },
+        { $set: { "myContribution.$": resource } }
+      );
+  
+      if (result.modifiedCount === 0) {
+        return res.status(404).send({ message: 'Contribution not found or already updated' });
+      }
+  
+      res.send({ message: 'Updated successfully', result });
+    } catch (error) {
+      console.error('Error updating:', error);
+      res.status(500).send({ message: 'An error occurred', error });
+    }
+  });
+  
+
+
+
+
+  // a user insert bookmarked data
   app.patch('/users/bookmark/:email', async (req, res) => {
     const { email } = req.params;
     const {  resource } = req.body;
@@ -102,11 +139,10 @@ async function run() {
 });
 
 
-//toooodoooooooooo: Problem -----------------------------------
+// delete user bookmark using id
 app.delete('/users/:email/bookmarks/:id', async (req, res) => {
   const email = req.params.email;
   const bookmarkId = req.params.id;
-  console.log(email, bookmarkId);
 
   try {
       const result = await usersCollection.updateOne(
@@ -129,7 +165,7 @@ app.delete('/users/:email/bookmarks/:id', async (req, res) => {
 
 
 
-    //course Update CSE
+    //course Update CSE=========================================================================
     app.patch('/courses/:courseCode', async (req, res) => {
       const { courseCode } = req.params;
       const { contentType, resource } = req.body;
@@ -155,11 +191,65 @@ app.delete('/users/:email/bookmarks/:id', async (req, res) => {
       }
   });
   
+
+
+  
    
+//edit course details with users
+app.patch('/courses/:courseCode/resources/:resourceId', async (req, res) => {
+  const { courseCode, resourceId } = req.params;
+  const {  contentType, resource } = req.body;
+
+  if (!['Playlist', 'Note', 'questionBank', 'other'].includes(contentType)) {
+      return res.status(400).send({ message: 'Invalid Content Name' });
+  }
+
+  try {
+      const filter = {
+          courseCode,
+          [`${contentType}.id`]: resourceId,
+          
+      };
+      const update = {
+          $set: {
+              [`${contentType}.$`]: resource
+          }
+      };
+      const result = await courseCollection.updateOne(filter, update);
+
+      if (result.matchedCount === 0) {
+          return res.status(404).send({ message: 'Resource or course not found' });
+      }
+
+      res.send({ message: 'Resource updated successfully', result });
+  } catch (err) {
+      res.status(500).send({ message: 'Internal server error', error: err });
+  }
+});
+
+
+
+
   app.get('/cseCourses',async(req, res) => {
     const result= await courseCollection.find().toArray();
    res.send(result);
   })
+
+
+
+
+
+  // Clubs ====================================================================================
+
+  app.get('/clubs',async(req, res) => {
+    const result= await clubsCollection.find().toArray();
+    res.send(result);
+  })
+
+
+
+
+
 
 
  
